1a9925aa3d3875fd3ae34307286867600eade0ef
Oiva Eskola <oiva.eskola@sc5.fi>
2012-03-23 14:30:57 +0200

    update jquery to version 1.7.2

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
   requirejs.config({
     paths: {
       backbone: 'ext/backbone-0.9.1.amd.min',
-      jquery: 'ext/jquery',
+      jquery: 'ext/jquery-1.7.2.min',
       underscore: 'ext/underscore-1.3.1.min'
     }
   });
